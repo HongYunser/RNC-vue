8ba8a328c5999ab02d81b7f2a00dd0439c903c6d
howie <360549145@qq.com>
2018-11-29 14:53:07 +0800

    格式化代码

--- check.js
+++ check.js
@@ -1,46 +1,47 @@
-const exec = require('child_process').exec
-const CLIEngine = require('eslint').CLIEngine
-const cli = new CLIEngine({})
+const exec = require('child_process').exec;
+const CLIEngine = require('eslint').CLIEngine;
+
+const cli = new CLIEngine({});
 function getErrorLevel(number) {
-    switch (number) {
-        case 2:
-            return 'error'
-        case 1:
-            return 'warn'
-        default:
-    }
-    return 'undefined'
+  switch (number) {
+    case 2:
+      return 'error';
+    case 1:
+      return 'warn';
+    default:
+  }
+  return 'undefined';
 }
-let pass = 0
+let pass = 0;
 exec('git diff --cached --name-only| grep .js$', (error, stdout) => {
-    if (stdout.length) {
-        const array = stdout.split('\n')
-        array.pop()
-        const results = cli.executeOnFiles(array).results
-        let errorCount = 0
-        let warningCount = 0
-        results.forEach((result) => {
-            errorCount += result.errorCount
-            warningCount += result.warningCount
-            if (result.messages.length > 0) {
-                console.log('\n')
-                console.log(result.filePath)
-                result.messages.forEach((obj) => {
-                    const level = getErrorLevel(obj.severity)
-                    console.log(`   ${obj.line}:${obj.column}  ${level}  ${obj.message}  ${obj.ruleId}`)
-                    pass = 1
-                })
-            }
-        })
-        if (warningCount > 0 || errorCount > 0) {
-            console.log(`\n   ${errorCount + warningCount} problems (${errorCount} ${'errors'} ${warningCount} warnings)`)
-        }
-        process.exit(pass)
+  if (stdout.length) {
+    const array = stdout.split('\n');
+    array.pop();
+    const results = cli.executeOnFiles(array).results;
+    let errorCount = 0;
+    let warningCount = 0;
+    results.forEach((result) => {
+      errorCount += result.errorCount;
+      warningCount += result.warningCount;
+      if (result.messages.length > 0) {
+        console.log('\n');
+        console.log(result.filePath);
+        result.messages.forEach((obj) => {
+          const level = getErrorLevel(obj.severity);
+          console.log(`   ${obj.line}:${obj.column}  ${level}  ${obj.message}  ${obj.ruleId}`);
+          pass = 1;
+        });
+      }
+    });
+    if (warningCount > 0 || errorCount > 0) {
+      console.log(`\n   ${errorCount + warningCount} problems (${errorCount} ${'errors'} ${warningCount} warnings)`);
     }
-    if (error !== null) {
-        console.log(`exec error: ${error}`)
-    }
-})
+    process.exit(pass);
+  }
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+});
 
 // 作者：du_4c0c
 // 链接：https://www.jianshu.com/p/072a96633479
--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -3,8 +3,8 @@ export default {
   name: 'Home',
   props: {
     msg: String,
-  }
-}
+  },
+};
 </script>
 <template src="./index.html"></template>
 <style scoped lang="scss" src="./index.scss"></style>
